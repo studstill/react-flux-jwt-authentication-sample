fb40ff94844895d3aeb7d4dcf1549d43d3005ec2
Jason Studstill <standstill64@gmail.com>
2015-08-24 21:45:28 -0700

    Fix indentation

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -27,21 +27,21 @@ export default class Signup extends React.Component {
       <div className="login jumbotron center-block">
         <h1>Signup</h1>
         <form role="form">
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" valueLink={this.linkState('user')} className="form-control" id="username" placeholder="Username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" valueLink={this.linkState('password')} className="form-control" id="password" ref="password" placeholder="Password" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="extra">Extra</label>
-          <input type="text" valueLink={this.linkState('extra')} className="form-control" id="password" ref="password" placeholder="Some extra information" />
-        </div>
-        <button type="submit" className="btn btn-default" onClick={this.signup.bind(this)}>Submit</button>
-      </form>
-    </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" valueLink={this.linkState('user')} className="form-control" id="username" placeholder="Username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" valueLink={this.linkState('password')} className="form-control" id="password" ref="password" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="extra">Extra</label>
+            <input type="text" valueLink={this.linkState('extra')} className="form-control" id="password" ref="password" placeholder="Some extra information" />
+          </div>
+          <button type="submit" className="btn btn-default" onClick={this.signup.bind(this)}>Submit</button>
+        </form>
+      </div>
     );
   }
 }
